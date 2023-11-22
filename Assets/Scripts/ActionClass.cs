c9673ca4085ef4331234aab608dcfb622f7175f5
Motoch007 <mosesmckeon@gmail.com>
2023-11-21 20:52:55 -0500

    Added the ROUGH action class Script
    I really hope I did this right cause It took me a bit to puzzle this out myself.

---
+++ Assets/Scripts/ActionClass.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionClass : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    // Action methods
+    public void Eat()
+    {
+        Debug.Log("Eating...");
+        // Add logic for eating here
+    }
+
+    public void Sleep()
+    {
+        Debug.Log("Sleeping...");
+        // Add logic for sleeping here
+    }
+
+    public void GatherFood()
+    {
+        Debug.Log("Gathering food...");
+        // Add logic for gathering food here
+    }
+
+    public void GatherWood()
+    {
+        Debug.Log("Gathering wood...");
+        // Add logic for gathering wood here
+    }
+
+    public void PlayGames()
+    {
+        Debug.Log("Playing games...");
+        // Add logic for playing games here
+    }
+
+    public void DanceAtFire()
+    {
+        Debug.Log("Dancing at fire...");
+        // Add logic for dancing at fire here
+    }
+
+}
