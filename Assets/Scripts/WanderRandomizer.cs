1c85b8da58e7d73f53dd4434a705bbebf293d263
Spyco03 <90427786+Spyco03@users.noreply.github.com>
2023-12-11 17:21:19 -0500

    Randomised Wandering

--- Assets/Scripts/WanderRandomizer.cs
+++ Assets/Scripts/WanderRandomizer.cs
@@ -5,11 +5,16 @@ using System.Threading;
 
 public class WanderRandomizer : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private void Start()
+    {
+        InvokeRepeating("MoveObject", 1.0f, 1.0f);
+    }
+    private void MoveObject()
     {
         Thread.Sleep(Random.Range(10,20));
-        float posx = Random.Range(-9, 9);
-        float posy = Random.Range(-5, 5);
+        float posx = Random.Range(-9, 9) + 0.5f;
+        float posy = Random.Range(-5, 5) + 0.5f;
         Vector3 newpositon = new Vector3(posx, posy, 0);
         transform.position = newpositon;
         //Debug.Log("Triggered");
